d832b14a634aa8a2d5eaabc8615c83260a2685be
Esra <esra.isildar@gmx.de>
2021-06-18 22:56:05 +0200

    anfang3.4

---
+++ Aufgabe3.4/client.ts
@@ -0,0 +1,33 @@
+namespace Aufgabe3_4 {
+
+    let buttonVerschicken: HTMLButtonElement = <HTMLButtonElement> document.getElementById("Versenden"); 
+    buttonVerschicken.addEventListener("click", versenden); 
+    let buttonAusgabe: HTMLButtonElement = <HTMLButtonElement> document.getElementById("Serveranfrage"); 
+    buttonAusgabe.addEventListener("click", serveranfrage);
+    let rueckgabe: HTMLDivElement = <HTMLDivElement> document.getElementById("Ausgabe");
+
+    async function versenden(): Promise<void> { 
+        let formData: FormData = new FormData (document.forms[0]);
+        let url: RequestInfo = "https://gissose2021esra.herokuapp.com";
+        url += "/Versenden";
+     
+        let  query: URLSearchParams = new URLSearchParams(<any> formData);
+        url = url + "?" + query.toString(); 
+        let antwort: Response = await fetch (url);
+        let ausgabe: string = await antwort.text();
+        console.log(ausgabe);    
+    }
+
+    async function serveranfrage(): Promise <void> { 
+        let formData: FormData = new FormData (document.forms[0]);
+        let url: RequestInfo = "https://gissose2021esra.herokuapp.com";
+        url += "/Serveranfrage";
+        let  query: URLSearchParams = new URLSearchParams(<any> formData);
+        url = url + "?" + query.toString();
+        let antwort: Response = await fetch (url);
+        let ausgabe: string = await antwort.text();
+        rueckgabe.innerHTML = ausgabe;
+    }
+
+
+}
---
+++ Aufgabe3.4/script.ts
@@ -0,0 +1,80 @@
+import * as Http from "http";
+import * as Url from "url";
+import * as Mongo from "mongodb";
+
+export namespace Aufgabe3_4 {
+
+
+let urlDB: string = "mongodb+srv://dbUser:<password>@isildaresra.6xflr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+let port: number = Number(process.env.PORT); 
+if (!port)
+    port = 8100;
+
+startServer(port); 
+
+
+
+function startServer (_port: number | string): void {
+    let server: Http.Server = Http. createServer();
+    console.log("Gestartet");
+    server.listen(_port);
+    server.addListener("request", handleRequest);
+
+}
+
+
+async function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): Promise <void> { 
+    console.log("Angekommen");
+    _response.setHeader("content-type", "text/html; charset=utf-8");
+    _response.setHeader("Access-Control-Allow-Origin", "*");
+
+    if (_request.url) {
+        let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
+        let pathname: string = <string>url.pathname;
+        let eingabe: Eingabe = {vorname: url.query.vorname + "", nachname: url.query.nachname + "", feedback: url.query.feedback + ""};
+
+        if (pathname == "/Versenden") { 
+        let daten: string = await abspeichern (urlDB, eingabe);
+        _response.write(daten);
+    }
+
+        else if (pathname == "/Serveranfrage") { 
+            let antwort: Eingabe[] = await dbAuslesen(urlDB);
+            console.log(antwort);
+            _response.write(JSON.stringify(antwort));
+        }     
+    }
+    _response.end();
+}
+    
+async function abspeichern(_url: string, _eingabe: Eingabe): Promise <string> {
+    let options: Mongo.MongoClientOptions = {useNewUrlParser: true, useUnifiedTopology: true};
+    let mongoClient: Mongo.MongoClient = new Mongo.MongoClient(_url, options);
+    await mongoClient.connect();
+
+    let infos: Mongo.Collection = mongoClient.db("Test").collection("Students");
+    infos.insertOne (_eingabe);
+    let antwort: string = "Gespeichert";
+    return antwort;
+}
+
+async function dbAuslesen(_url: string): Promise <Eingabe[]> {
+    let options: Mongo.MongoClientOptions = {useNewUrlParser: true, useUnifiedTopology: true};
+    let mongoClient: Mongo.MongoClient = new Mongo.MongoClient(_url, options);
+    await mongoClient.connect();
+
+    let infos: Mongo.Collection = mongoClient.db("Test").collection("Students");
+    let cursor: Mongo.Cursor = infos.find ();
+    let result: Eingabe[] = await cursor.toArray();
+    return result;
+
+}
+
+
+interface Eingabe {
+    vorname: string;
+    nachname: string;
+    feedback: string;
+}
+
+}
